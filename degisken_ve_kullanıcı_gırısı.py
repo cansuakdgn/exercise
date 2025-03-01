67add4b09f7c96abb76b371c1e20a7f05784c534
Cansu Akdogan <cansuakdgn0@gmail.com>
2025-03-01 14:29:33 +0300

    Odev tamamlandı.

---
+++ degisken_ve_kullanıcı_gırısı.py
@@ -0,0 +1,10 @@
+# Kullanıcıdan adını, soyadını ve yaşını alan
+# bir Python programı yazın. Alınan bilgileri şu formatta
+# ekrana yazdırın:"Merhaba [Ad Soyad], [Yaş] yaşındasınız.
+#  Hoş geldiniz!"
+
+ad=input("Adınızı giriniz:")
+soyad=input("Soyadınızı giriniz:")
+yas=input("Yasınızı giriniz:")
+
+print(f"Merhaba {ad} {soyad}, {yas} yasındasınız. Hoşgeldiniz!")
